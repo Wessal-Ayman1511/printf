0144c99e4e3bfd114c8ab9d88ba17674cfd2a9e5
Wessal Ayman <wessalayman237@gmail.com>
2023-10-14 08:39:21 -0700

    a

---
+++ 0_printf.c
@@ -0,0 +1,49 @@
+#include "main.h"
+/**
+ * _printf- function that print
+ * @format: input
+ * Return: num of char
+*/
+int _printf(const char *format, ...)
+{
+	int c = 0;
+	va_list  ptr;
+
+	if (!format)
+		return (-1);
+	va_start(ptr, format);
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			write(1, format, 1);
+			c++;
+		}
+		else
+		{
+			format++;
+			if (*format == '\0')
+				break;
+			if (*format == '%')
+			{
+				write(1, format, 1);
+				c++;
+			}
+			else if (*format == 'c')
+			{
+				char ch = va_arg(ptr, int);
+
+				write(1, &ch, 1);
+				c++;
+			}
+			else if (*format == 's')
+			{
+				char *s = va_arg(ptr, char*);
+
+				write(1, s, _strlen(s));
+				c += _strlen(s);
+			}
+		} format++;
+	} va_end(ptr);
+	return (c);
+}
